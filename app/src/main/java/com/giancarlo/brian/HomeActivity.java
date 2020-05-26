3a83db295bef9d8f85d1d84bf03ec1b4559bb21b
gian <giancarlo.7991@gmail.com>
2020-05-26 17:29:14 -0500

    add-sound

--- app/src/main/java/com/giancarlo/brian/HomeActivity.java
+++ app/src/main/java/com/giancarlo/brian/HomeActivity.java
@@ -2,13 +2,47 @@ package com.giancarlo.brian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    TextView txtFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Bundle extra = getIntent().getExtras();
+        String fecha = extra.getString("fecha");
+        setTitle(fecha);
+
+        txtFecha = (TextView)findViewById(R.id.textView_fecha);
+        txtFecha.setText(fecha);
+
+        mp = MediaPlayer.create(this,R.raw.afirmkick);
+        mp.start();
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mp.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mp.start();
+    }
+
 }
--- app/src/main/java/com/giancarlo/brian/MainActivity.java
+++ app/src/main/java/com/giancarlo/brian/MainActivity.java
@@ -2,14 +2,59 @@ package com.giancarlo.brian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText txtNombre,txtContraseña;
+    Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        txtNombre = (EditText)findViewById(R.id.editText_nombre);
+        txtContraseña = (EditText)findViewById(R.id.editText_contraseña);
+        btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent HomeItemt = new Intent(this,HomeActivity.class);
+
+        String nombre = txtNombre.getText().toString();
+        String contraseña = txtContraseña.getText().toString();
+        String fecha = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
+        if(!contraseña.isEmpty() && !nombre.isEmpty()){
+            Boolean acceso = contraseña.equals("3937") && nombre.equals("brian");
+            if(acceso){
+                HomeItemt.putExtra("fecha",fecha);
+                startActivity(HomeItemt);
+                finish();
+            }else{
+                txtNombre.setError("Nombre o Contraseña Erronea!");
+                txtNombre.setText("");
+                txtContraseña.setText("");
+            }
+
+        }else{
+            txtNombre.setError("Ingrese Nombre!");
+            txtContraseña.setError("Ingrese Contraseña!");
+        }
+
+
     }
 }
