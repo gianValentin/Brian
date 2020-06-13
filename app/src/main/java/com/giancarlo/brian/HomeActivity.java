dbcea0a10bd8131f21b95abeb1a627f2008a1bc6
giancarlo <giancarlo.7991@gmail.com>
2020-06-12 21:07:42 -0500

    implemet-Carucel-img

--- app/src/main/java/com/giancarlo/brian/HomeActivity.java
+++ app/src/main/java/com/giancarlo/brian/HomeActivity.java
@@ -4,10 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageClickListener;
+import com.synnapps.carouselview.ImageListener;
 
+public class HomeActivity extends AppCompatActivity{
+
+    private int[] img = new int[]{R.drawable.carrusel05,R.drawable.carrusel06,R.drawable.carrusel07};
+    private String[] nameImg = new String[]{"carrusel05","carrusel06","carrusel07"};
     MediaPlayer mp;
     TextView txtFecha;
 
@@ -20,11 +28,27 @@ public class HomeActivity extends AppCompatActivity {
         String fecha = extra.getString("fecha");
         setTitle(fecha);
 
-        txtFecha = (TextView)findViewById(R.id.textView_fecha);
-        txtFecha.setText(fecha);
+        //txtFecha = (TextView)findViewById(R.id.textView_fecha);
+        //txtFecha.setText(fecha);
 
         mp = MediaPlayer.create(this,R.raw.afirmkick);
         mp.start();
+
+        //Iniciar Carrusel
+        CarouselView carousel = (CarouselView)findViewById(R.id.cvHome);
+        carousel.setPageCount(img.length);
+        carousel.setImageListener(new ImageListener() {
+            @Override
+            public void setImageForPosition(int position, ImageView imageView) {
+                imageView.setImageResource(img[position]);
+            }
+        });
+        carousel.setImageClickListener(new ImageClickListener() {
+            @Override
+            public void onClick(int position) {
+                Toast.makeText(HomeActivity.this, nameImg[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -44,5 +68,4 @@ public class HomeActivity extends AppCompatActivity {
         super.onResume();
         mp.start();
     }
-
 }
